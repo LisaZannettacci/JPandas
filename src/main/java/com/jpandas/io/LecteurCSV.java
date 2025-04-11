d3fd64516b9481303aa09e7393cb11cfb2272380
Justine38100 <justinereat@gmail.com>
2025-04-11 17:48:28 +0200

    Comment la génération html a fonctionné: modification des autres fichiers!

--- src/main/java/com/jpandas/io/LecteurCSV.java
+++ src/main/java/com/jpandas/io/LecteurCSV.java
@@ -10,9 +10,15 @@ import com.jpandas.core.DataFrame;
 import com.jpandas.core.Series;
 
 /**
- * La classe LecteurCSV est utilisee pour lire les donnees d'un fichier CSV
+ * La classe LecteurCSV est utilis&eacute;e pour lire les donn&eacute;es d'un fichier CSV
  * et les convertir en un DataFrame, ou chaque colonne du CSV devient une Series
  * dans le DataFrame.
+ * <br>
+ * <b>Exemple :</b>
+ * <pre>
+ * LecteurCSV lecteur = new LecteurCSV();
+ * DataFrame df = new DataFrame("chemin/vers/fichier.csv");
+ * </pre>
  * 
  * @author Lisa ZANNETTACCI, REAT Justine
  * @version 1.0
@@ -21,34 +27,37 @@ import com.jpandas.core.Series;
 public class LecteurCSV {
 
     /**
-     * Constructeur par defaut de la classe LecteurCSV.
-     * Ce constructeur est utilise pour creer une instance de la classe LecteurCSV sans parametres.
-     * 
+     * Constructeur par d&eacute;faut de la classe LecteurCSV.
+     * Ce constructeur est utilis&eacute; pour cr&eacute;er une instance de la classe LecteurCSV sans param&egrave;tres.
+     * <br>
+     * Ce constructeur peut &ecirc;tre vide si aucune initialisation particuli&egrave;re n'est requise.
      */
     public LecteurCSV() {
-        // Le constructeur par defaut peut etre vide si aucune initialisation particuliere n'est requise
+        // Le constructeur par d&eacute;faut peut &ecirc;tre vide si aucune initialisation particuli&egrave;re n'est requise
     }
 
     /**
-     * Cette methode lit toutes les lignes d'un fichier CSV et cree un DataFrame.
-     * Principe: On commence par lire la premiere ligne (les en-tetes) et on cree une Series pour chaque colonne du DataFrame.
-     * Ensuite, on parcourt les lignes suivantes (les donnees) et on ajoute chaque valeur dans la colonne appropriee.
-     * Si le csv ne contient pas de colonne "Index", on l'ajoute.
+     * Cette m&eacute;thode lit toutes les lignes d'un fichier CSV et cr&eacute;e un DataFrame.
+     * <p>
+     * Principe : On commence par lire la premi&egrave;re ligne (les en-t&ecirc;tes) et on cr&eacute;e une Series pour chaque colonne du DataFrame.
+     * Ensuite, on parcourt les lignes suivantes (les donn&eacute;es) et on ajoute chaque valeur dans la colonne appropri&eacute;e.
+     * Si le CSV ne contient pas de colonne "Index", on l'ajoute.
+     * </p>
      * 
      * @param path Chemin du fichier
-     * @param dataframe Le dataframe qui va contenir les donnees de notre CSV
+     * @param dataframe Le dataframe qui va contenir les donn&eacute;es de notre CSV
      */
     public static void parseCSV(String path, DataFrame dataframe) {
         try {
             List<String> lignes = Files.readAllLines(Paths.get(path));
     
             if (lignes.size() < 2) {
-                throw new IllegalArgumentException("Le fichier CSV est mal forme.");
+                throw new IllegalArgumentException("Le fichier CSV est mal form&eacute;.");
             }
     
             String[] enTetes = lignes.get(0).split(",");
     
-            // Verifie si une colonne Index est deja presente
+            // V&eacute;rifie si une colonne Index est d&eacute;j&agrave; pr&eacute;sente
             boolean indexColonne = false;
             for (String enTete : enTetes) {
                 if (enTete.equalsIgnoreCase("Index")) {
@@ -57,12 +66,12 @@ public class LecteurCSV {
                 }
             }
     
-            // On cree TOUTES les colonnes du fichier, y compris "Index" s’il est deja la
+            // On cr&eacute;e TOUTES les colonnes du fichier, y compris "Index" s&rsquo;il est d&eacute;j&agrave; l&agrave;
             for (String enTete : enTetes) {
                 dataframe.colonne.put(enTete, new Series<>(new ArrayList<>()));
             }
     
-            // On parcourt les lignes de donnees et on les ajoute dans les bonnes colonnes
+            // On parcourt les lignes de donn&eacute;es et on les ajoute dans les bonnes colonnes
             for (int i = 1; i < lignes.size(); i++) {
                 String[] valeurs = lignes.get(i).split(",");
                 for (int j = 0; j < enTetes.length; j++) {
