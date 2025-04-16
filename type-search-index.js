00ff1546866cf88e448e2a71f1000236ecfc0c57
LisaZannettacci <151721804+LisaZannettacci@users.noreply.github.com>
2025-04-16 12:55:37 +0000

    Deploying to gh-pages from @ LisaZannettacci/JPandas@2d03c7e5e9b808209a99fdb2e30dd6ef79684d4d 🚀

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.jpandas.core","l":"DataFrame"},{"p":"com.jpandas.io","l":"LecteurCSV"},{"p":"com.jpandas","l":"Main"},{"p":"com.jpandas.core","l":"Series"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.jpandas.core","l":"DataFrame"},{"p":"com.jpandas","l":"JPandas"},{"p":"com.jpandas.io","l":"LecteurCSV"},{"p":"com.jpandas.core","l":"Series"}];updateSearchResults();
